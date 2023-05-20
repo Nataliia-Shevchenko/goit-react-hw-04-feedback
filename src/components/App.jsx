a35ec96e49dfcd1c992c77579bca043241ef01c2
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-05-20 22:15:43 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Section from 'components/Section';
 import FeedbackOptions from 'components/FeedbackOptions';
 import Statistics from 'components/Statistics';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleLeaveFeedback = e => {
-    this.setState(prevState => ({
-      [e.target.name]: prevState[e.target.name] + 1,
-    }));
-  };
+  // useEffect(() => {
 
-  countTotalFeedback = (good, neutral, bad) => {
+  // }, [good, neutral, bad]);
+
+  const handleLeaveFeedback = e => {
+    if (e.target.name === good) {
+      setGood(prev => {
+        return prev + 1;
+      });
+    } else if (e.target.name === neutral) {
+      setNeutral(prev => prev + 1);
+    } else if (e.target.name === good) {
+      setBad(prev => prev + 1);
+    }
+  };
+  // this.setState(prevState => ({
+  //     [e.target.name]: prevState[e.target.name] + 1,
+  //   }));
+  // };
+
+  const countTotalFeedback = (good, neutral, bad) => {
     return good + neutral + bad;
-  }
+  };
 
-  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+  const countPositiveFeedbackPercentage = (good, neutral, bad) => {
     return Math.round((good * 100) / (good + neutral + bad));
-  }
+  };
 
+  const TotalFeedback = countTotalFeedback(good, neutral, bad);
+  const PositiveFeedbackPercentage = countPositiveFeedbackPercentage(
+    good,
+    neutral,
+    bad
+  );
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const TotalFeedback = this.countTotalFeedback(good, neutral, bad);
-    const PositiveFeedbackPercentage = this.countPositiveFeedbackPercentage(good, neutral, bad);
-   
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.handleLeaveFeedback}
-        ></FeedbackOptions>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={TotalFeedback}
-          positivePercentage={PositiveFeedbackPercentage}
-        ></Statistics>
-      </Section>
-    );
-  }
-}
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={handleLeaveFeedback}
+      ></FeedbackOptions>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={(TotalFeedback)}
+        positivePercentage={PositiveFeedbackPercentage}
+      ></Statistics>
+    </Section>
+  );
+};
 
 export default App;
